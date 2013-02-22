64cc16b1872ed068deb8d7f0ababdbf92ba4e223
Andrew DiMichele <backflip@gmail.com>
2013-02-22 10:25:25 -0800

    Added stubs for the entire Contacts API

--- lib/hubspot/contact.rb
+++ lib/hubspot/contact.rb
@@ -2,12 +2,30 @@ require 'hubspot/utils'
 require 'httparty'
 
 module Hubspot
+  #
+  # HubSpot Contacts API
+  #
+  # {https://developers.hubspot.com/docs/endpoints#contacts-api}
+  #
   class Contact
     GET_CONTACT_BY_EMAIL_PATH = "/contacts/v1/contact/email/:contact_email/profile"
     GET_CONTACT_BY_ID_PATH = "/contacts/v1/contact/vid/:contact_id/profile"
     UPDATE_CONTACT_PATH = "/contacts/v1/contact/vid/:contact_id/profile"
 
     class << self
+      # TODO: Creates a new contact
+      # {https://developers.hubspot.com/docs/methods/contacts/create_contact}
+      # @param email [Hash] unique email of the new contact
+      # @param params [Hash] hash of properties to set on the contact
+      # @return [Hubspot::Contact] the newly created contact
+      def create!(email, params={})
+        raise NotImplementedError
+      end
+
+      # Finds a contact by email
+      # {https://developers.hubspot.com/docs/methods/contacts/get_contact_by_email}
+      # @param email [String] the email of the contact to find
+      # @return [Hubspot::Contact, nil] the contact found or nil
       def find_by_email(email)
         url = Hubspot::Utils.generate_url(GET_CONTACT_BY_EMAIL_PATH, {contact_email: email})
         resp = HTTParty.get(url)
@@ -18,6 +36,9 @@ module Hubspot
         end
       end
 
+      # Finds a contact by vid
+      # @param vid [String] the vid of the contact to find
+      # @return [Hubspot::Contact, nil] the contact found or nil
       def find_by_id(vid)
         url = Hubspot::Utils.generate_url(GET_CONTACT_BY_ID_PATH, {contact_id: vid})
         resp = HTTParty.get(url)
@@ -26,6 +47,46 @@ module Hubspot
         else
           nil
         end
+      end
+
+      # TODO: Finds a contact by its User Token (hubspotutk cookie value)
+      # {https://developers.hubspot.com/docs/methods/contacts/get_contact_by_utk}
+      # @param utk [String] hubspotutk cookie value
+      # @return [Hubspot::Contact, nil] the contact found or nil
+      def find_by_utk(utk)
+        raise NotImplementedError
+      end
+
+      # TODO: Get all contacts
+      # {https://developers.hubspot.com/docs/methods/contacts/get_contacts}
+      # @param count [Fixnum] number of contacts per page; max 100
+      # @return [Hubspot::ContactCollection] the paginated collection of contacts
+      def all(count=100)
+        raise NotImplementedError
+      end
+
+      # TODO: Get recently updated and created contacts
+      # {https://developers.hubspot.com/docs/methods/contacts/get_recently_updated_contacts}
+      # @param count [Fixnum] number of contacts per page; max 100
+      # @return [Hubspot::ContactCollection] the paginated collection of contacts
+      def recent(count=100)
+        raise NotImplementedError
+      end
+
+      # TODO: Search for contacts by various crieria
+      # {https://developers.hubspot.com/docs/methods/contacts/search_contacts}
+      # @param query [String] The search term for what you're searching for
+      # @param count [Fixnum] number of contacts per page; max 100
+      # @return [Hubspot::ContactCollection] the collection of contacts; no pagination
+      def search(query, count=100)
+        raise NotImplementedError
+      end
+
+      # TODO: Get statistics about all contacts
+      # {https://developers.hubspot.com/docs/methods/contacts/get_contact_statistics}
+      # @return [Hash] hash of statistics
+      def statistics
+        raise NotImplementedError
       end
     end
 
@@ -45,6 +106,10 @@ module Hubspot
       @properties["email"]
     end
 
+    # Updates the properties of a contact
+    # {https://developers.hubspot.com/docs/methods/contacts/update_contact}
+    # @param params [Hash] hash of properties to update
+    # @return [Hubspot::Contact] self
     def update!(params)
       params.stringify_keys!
       url = Hubspot::Utils.generate_url(UPDATE_CONTACT_PATH, {contact_id: vid})
@@ -53,6 +118,13 @@ module Hubspot
       raise(Hubspot::RequestError.new(resp.response)) unless resp.success?
       @properties.merge!(params)
       self
+    end
+
+    # TODO: Archives the contact in hubspot
+    # {https://developers.hubspot.com/docs/methods/contacts/delete_contact}
+    # @return [nil]
+    def destroy!
+      raise NotImplementedError
     end
   end
 end
