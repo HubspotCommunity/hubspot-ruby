a0d368348b92b5f35fd4f92cc5af740521f4a096
Paulo Diniz <paulo.rc.diniz@gmail.com>
2014-12-08 11:38:40 -0200

    creating and finding deals

--- spec/lib/hubspot/deal_spec.rb
+++ spec/lib/hubspot/deal_spec.rb
@@ -15,4 +15,24 @@ describe Hubspot::Deal do
     its (:portal_id) { should == 62515 }
     its (:deal_id) { should == 3 }
   end
+
+  describe ".create!" do
+    cassette "deal_create"
+    subject { Hubspot::Deal.create!(62515, [8954037], [27136], {}) }
+    its(:deal_id)     { should_not be_nil }
+    its(:portal_id)   { should eql 62515 }
+    its(:company_ids) { should eql [8954037]}
+    its(:vids)        { should eql [27136]}
+  end
+
+  describe ".find" do
+    cassette "deal_find"
+    let(:deal) {Hubspot::Deal.create!(62515, [8954037], [27136], {})}
+
+    it 'must find by the deal id' do
+      find_deal = Hubspot::Deal.find(deal.deal_id)
+      find_deal.deal_id.should eql deal.deal_id
+    end
+  end
+
 end
