e41696e25b1cc119edf176160c765fb58ca9cbbd
santig <garcia.santiago@gmail.com>
2017-06-23 16:54:22 -0500

    adding owner id to owner Ids in engagement stask

--- lib/hubspot/engagement.rb
+++ lib/hubspot/engagement.rb
@@ -210,7 +210,8 @@ module Hubspot
             type: 'TASK'
           },
           associations: {
-            contactIds: [contact_id]
+            contactIds: [contact_id],
+            ownerIds: [owner_id]
           },
           metadata: {
             body: task_body,
