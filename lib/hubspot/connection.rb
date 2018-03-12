2de261d9622aebac052ac6490550dad9e1e43313
itaivaks <itai@platterz.ca>
2018-03-12 14:03:29 -0400

    add support for timeline events

--- lib/hubspot/connection.rb
+++ lib/hubspot/connection.rb
@@ -30,6 +30,14 @@ module Hubspot
         response.parsed_response
       end
 
+      def put_json_with_token(path, token, opts)
+        url = generate_url_using_oauth(path, opts[:params])
+        response = put(url, body: opts[:body].to_json, headers: { 'Content-Type' => 'application/json', 'Authorization' => "Bearer #{token}" }, format: :json)
+        log_request_and_response url, response, opts[:body]
+        raise(Hubspot::RequestError.new(response)) unless response.success?
+        response.parsed_response
+      end
+
       def delete_json(path, opts)
         url = generate_url(path, opts)
         response = delete(url, format: :json)
@@ -50,6 +58,33 @@ module Hubspot
         params = params.clone
         base_url = options[:base_url] || Hubspot::Config.base_url
         params["hapikey"] = Hubspot::Config.hapikey unless options[:hapikey] == false
+
+        if path =~ /:portal_id/
+          Hubspot::Config.ensure! :portal_id
+          params["portal_id"] = Hubspot::Config.portal_id if path =~ /:portal_id/
+        end
+
+        params.each do |k,v|
+          if path.match(":#{k}")
+            path.gsub!(":#{k}", CGI.escape(v.to_s))
+            params.delete(k)
+          end
+        end
+        raise(Hubspot::MissingInterpolation.new("Interpolation not resolved")) if path =~ /:/
+
+        query = params.map do |k,v|
+          v.is_a?(Array) ? v.map { |value| param_string(k,value) } : param_string(k,v)
+        end.join("&")
+
+        path += path.include?('?') ? '&' : "?" if query.present?
+        base_url + path + query
+      end
+
+      def generate_url_using_oauth(path, params={}, options={})
+        Hubspot::Config.ensure! :hapikey
+        path = path.clone
+        params = params.clone
+        base_url = options[:base_url] || Hubspot::Config.base_url
 
         if path =~ /:portal_id/
           Hubspot::Config.ensure! :portal_id
