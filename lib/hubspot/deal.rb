a884f9967a18b63c8827df8008a5ccf485de5191
Matt Anderson <matt.anderson@qadium.com>
2018-04-05 00:40:51 -0700

    Adding Deal find_by_contract
    * Created Hubspot::Deal::find_by_association to find deals via contact or company
    * Changed Hubspot::Deal::find_by_company to use Hubspot::Deal::find_by_association
    * Changed Hubspot::Deal::find_by_contact to use Hubspot::Deal::find_by_association

--- lib/hubspot/deal.rb
+++ lib/hubspot/deal.rb
@@ -47,7 +47,7 @@ module Hubspot
          object_ids = (company_ids.any? ? company_ids : vids).join('&id=')
          Hubspot::Connection.put_json(ASSOCIATE_DEAL_PATH, params: { deal_id: deal_id, OBJECTTYPE: objecttype, objectId: object_ids}, body: {})
        end
- 
+
 
       def find(deal_id)
         response = Hubspot::Connection.get_json(DEAL_PATH, { deal_id: deal_id })
@@ -62,14 +62,35 @@ module Hubspot
         response = Hubspot::Connection.get_json(RECENT_UPDATED_PATH, opts)
         response['results'].map { |d| new(d) }
       end
-      
+
       # Find all deals associated to a company
       # {http://developers.hubspot.com/docs/methods/deals/get-associated-deals}
       # @param company [Hubspot::Company] the company
       # @return [Array] Array of Hubspot::Deal records
       def find_by_company(company)
+        find_by_association company
+      end
+
+      # Find all deals associated to a contact
+      # {http://developers.hubspot.com/docs/methods/deals/get-associated-deals}
+      # @param contact [Hubspot::Contact] the contact
+      # @return [Array] Array of Hubspot::Deal records
+      def find_by_contact(contact)
+        find_by_assocation contact
+      end
+
+      # Find all deals associated to a contact or company
+      # {http://developers.hubspot.com/docs/methods/deals/get-associated-deals}
+      # @param object [Hubspot::Contact || Hubspot::Company] a contact or company
+      # @return [Array] Array of Hubspot::Deal records
+      def find_by_association(object)
         path = ASSOCIATED_DEAL_PATH
-        params = { objectType: :company, objectId: company.vid }
+        type = case object
+            when Hubspot::Company then :company
+            when Hubspot::Contact then :contact
+            else raise Hubspot::InvalidParams, 'Instance type not supported'
+            end
+        params = { objectType: type, objectId: object.vid }
         response = Hubspot::Connection.get_json(path, params)
         response["results"].map { |deal_id| find(deal_id) }
       end
