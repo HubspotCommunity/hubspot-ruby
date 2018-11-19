c105b3d7023abb2b057d338f840fe1c6e5861b98
Stephanie Viccari <stephviccari@gmail.com>
2018-11-19 10:57:35 -0500

    Address Travis test failures
    Summary:
    Due to a timezone issue, two tests in the `blog_spec` were passing
    locally but failing consistently on Travis
    
    The requests being issued to the `blog-posts` endpoint includes
    a timestamp that limits the response to blog posts created in the
    last two months. VCR records the timestamp in the URL and will
    include this value when matching future requests against
    recorded requests If VCR can't find a URL that matches the
    request, a new real request is issued.
    
    So why were the tests passing locally? The tests will pass locally
    for anyone in the America/New_York timezone since the recorded
    requests were generated using the America/New_York time zone.
    Travis is failing on these tests because Travis uses the UTC
    time zone.
    
    This PR:
    - Replaces `local` with `utc` to ensure consistency across time zones
    - Updates the test assertions check the correct request is being made,
       rather than validating the API returned the expected values.
    - Removes or updates tests marked as 'pending'
    - Refactors to remove the use of `its` (which was deprecated with the
      release of RSpec 3) and `lets`

--- spec/lib/hubspot/blog_spec.rb
+++ spec/lib/hubspot/blog_spec.rb
@@ -1,18 +1,9 @@
 require 'timecop'
 
 describe Hubspot do
-  let(:example_blog_hash) do
-    VCR.use_cassette("blog_list", record: :none) do
-      url = Hubspot::Connection.send(:generate_url, Hubspot::Blog::BLOG_LIST_PATH)
-      resp = HTTParty.get(url, format: :json)
-      resp.parsed_response["objects"].first
-    end
-  end
-  let(:created_range_params) { { created__gt: false, created__range: (Time.now..Time.now + 2.years)  } }
-
   before do
     Hubspot.configure(hapikey: "demo")
-    Timecop.freeze(Time.local(2012, 'Oct', 10))
+    Timecop.freeze(Time.utc(2012, 'Oct', 10))
   end
 
   after do
@@ -20,97 +11,140 @@ describe Hubspot do
   end
 
   describe Hubspot::Blog do
-
     describe ".list" do
-      cassette "blog_list"
-      let(:blog_list) { Hubspot::Blog.list }
+      it "returns a list of blogs" do
+        VCR.use_cassette("blog_list", record: :none) do
+          result = Hubspot::Blog.list
 
-      it "should have a list of blogs" do
-        blog_list.count.should be(1)
+          assert_requested :get, hubspot_api_url("/content/api/v2/blogs?hapikey=demo")
+          expect(result).to be_kind_of(Array)
+          expect(result.first).to be_a(Hubspot::Blog)
+        end
       end
     end
 
     describe ".find_by_id" do
-      cassette "blog_list"
+      it "retrieves a blog by id" do
+        VCR.use_cassette("blog_list", record: :none) do
+          id = 351076997
+          result = Hubspot::Blog.find_by_id(id)
 
-      it "should have a list of blogs" do
-        blog = Hubspot::Blog.find_by_id(351076997)
-        blog["id"].should eq(351076997)
+          assert_requested :get, hubspot_api_url("/content/api/v2/blogs/#{id}?hapikey=demo")
+          expect(result).to be_a(Hubspot::Blog)
+        end
       end
     end
 
-    describe "#initialize" do
-      subject{ Hubspot::Blog.new(example_blog_hash) }
-      its(["name"]) { should == "API Demonstration Blog" }
-      its(["id"])   { should == 351076997 }
+    describe "#[]" do
+      it "returns the value for the given key" do
+        data = {
+          "id" => 123,
+          "name" => "Demo",
+        }
+        blog = Hubspot::Blog.new(data)
+
+        expect(blog["id"]).to eq(data["id"])
+        expect(blog["name"]).to eq(data["name"])
+      end
+
+      context "when the value is unknown" do
+        it "returns nil" do
+          blog = Hubspot::Blog.new({})
+
+          expect(blog["nope"]).to be_nil
+        end
+      end
     end
 
     describe "#posts" do
-      cassette "one_month_blog_posts_filter_state"
-      let(:blog) { Hubspot::Blog.new(example_blog_hash) }
+      it "returns published blog posts created in the last 2 months" do
+        VCR.use_cassette("blog_posts/all_blog_posts", record: :none) do
+          blog_id = 123
+          created_gt = timestamp_in_milliseconds(Time.now - 2.months)
+          blog = Hubspot::Blog.new({ "id" => blog_id })
 
-      describe "can be filtered by state" do
+          result = blog.posts
 
-        it "should filter the posts to published by default" do
-          pending 'This test does not pass reliably'
-          blog.posts.length.should be(14)
-        end
-
-        it "should validate the state is a valid one" do
-          expect { blog.posts('invalid') }.to raise_error(Hubspot::InvalidParams)
-        end
-
-        it "should allow draft posts if specified" do
-          blog.posts({ state: false }.merge(created_range_params)).length.should be > 0
+          assert_requested :get, hubspot_api_url("/content/api/v2/blog-posts?content_group_id=#{blog_id}&created__gt=#{created_gt}&hapikey=demo&order_by=-created&state=PUBLISHED")
+          expect(result).to be_kind_of(Array)
         end
       end
 
-      describe "can be ordered" do
-        it "created at descending is default" do
-          created_timestamps = blog.posts(created_range_params).map { |post| post['created'] }
-          expect(created_timestamps.sort.reverse).to eq(created_timestamps)
-        end
+      it "includes given parameters in the request" do
+        VCR.use_cassette("blog_posts/filter_blog_posts", record: :none) do
+          blog_id = 123
+          created_gt = timestamp_in_milliseconds(Time.now - 2.months)
+          blog = Hubspot::Blog.new({ "id" => 123 })
 
-        it "by created ascending" do
-          pending
-          created_timestamps = blog.posts({order_by: '+created'}.merge(created_range_params)).map { |post| post['created'] }
-          expect(created_timestamps.sort).to eq(created_timestamps)
+          result = blog.posts({ state: "DRAFT" })
+
+          assert_requested :get, hubspot_api_url("/content/api/v2/blog-posts?content_group_id=#{blog_id}&created__gt=#{created_gt}&hapikey=demo&order_by=-created&state=DRAFT")
+          expect(result).to be_kind_of(Array)
         end
       end
 
-      it "can set a page size" do
-        pending 'Not working'
-        blog.posts({limit: 10}.merge(created_range_params)).length.should be(10)
+      it "raises when given an unknown state" do
+        blog = Hubspot::Blog.new({})
+
+        expect {
+          blog.posts({ state: "unknown" })
+        }.to raise_error(Hubspot::InvalidParams, "State parameter was invalid")
       end
     end
   end
 
   describe Hubspot::BlogPost do
-    cassette "blog_posts"
+    describe "#created_at" do
+      it "returns the created timestamp as a Time" do
+        timestamp = timestamp_in_milliseconds(Time.now)
+        blog_post = Hubspot::BlogPost.new({ "created" => timestamp })
 
-    let(:example_blog_post) do
-      VCR.use_cassette("one_month_blog_posts_filter_state", record: :none) do
-        blog = Hubspot::Blog.new(example_blog_hash)
-        blog.posts(created_range_params).first
+        expect(blog_post.created_at).to eq(Time.at(timestamp/1000))
       end
     end
 
-    it "should have a created_at value specific method" do
-      expect(example_blog_post.created_at).to eq(Time.at(example_blog_post['created'] / 1000))
-    end
+    describe ".find_by_blog_post_id" do
+      it "retrieves a blog post by id" do
+        VCR.use_cassette "blog_posts" do
+          blog_post_id = 422192866
 
-    it "can find by blog_post_id" do
-      blog = Hubspot::BlogPost.find_by_blog_post_id(422192866)
-      expect(blog['id']).to eq(422192866)
-    end
+          result = Hubspot::BlogPost.find_by_blog_post_id(blog_post_id)
 
-    context 'containing a topic' do
-      # 422192866 contains a topic
-      let(:blog_with_topic) { Hubspot::BlogPost.find_by_blog_post_id(422192866) }
-
-      it "should return topic objects" do
-        expect(blog_with_topic.topics.first.is_a?(Hubspot::Topic)).to be(true)
+          assert_requested :get, hubspot_api_url("/content/api/v2/blog-posts/#{blog_post_id}?hapikey=demo")
+          expect(result).to be_a(Hubspot::BlogPost)
+        end
       end
     end
+
+    describe "#topics" do
+      it "returns the list of topics" do
+        VCR.use_cassette "blog_posts" do
+          blog_post = Hubspot::BlogPost.find_by_blog_post_id(422192866)
+
+          topics = blog_post.topics
+
+          expect(topics).to be_kind_of(Array)
+          expect(topics.first).to be_a(Hubspot::Topic)
+        end
+      end
+
+      context "when the blog post does not have topics" do
+        it "returns an empty list" do
+          blog_post = Hubspot::BlogPost.new({ "topic_ids" => [] })
+
+          topics = blog_post.topics
+
+          expect(topics).to be_empty
+        end
+      end
+    end
+  end
+
+  def hubspot_api_url(path)
+    URI.join(Hubspot::Config.base_url, path)
+  end
+
+  def timestamp_in_milliseconds(time)
+    time.to_i * 1000
   end
 end
