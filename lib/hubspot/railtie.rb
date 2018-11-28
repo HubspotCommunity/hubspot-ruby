40a1be4c53ac3ed7fbb71b09476da650e7aed4f5
Stephanie Viccari <stephviccari@gmail.com>
2018-11-28 10:55:56 -0500

    Remove unused extension of Railtie to import rake tasks
    Summary:
    Previously, we were importing 'properties.rake' task for end users.
    This file was renamed to 'hubspot.rake', which led to users encountering
    the LoadError "cannot load such file" since the import referenced a file
    that no longer existed.
    
    Rather than add the import back to reference the correct file,
    'hubspot.rake', this change removes the railtie extension as we discuss
    the future of 'hubspot.rake' and if we wish to continue supporting it or
    remove it.

--- lib/hubspot/railtie.rb
+++
@@ -1,9 +0,0 @@
-require 'hubspot-ruby'
-require 'rails'
-module Hubspot
-  class Railtie < Rails::Railtie
-    rake_tasks do
-      spec = Gem::Specification.find_by_name('hubspot-ruby')
-    end
-  end
-end
