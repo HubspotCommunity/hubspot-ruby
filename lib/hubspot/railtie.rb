f938915d0e431bae51df065c5168ddeb2be1beab
Stephanie Viccari <stephviccari@gmail.com>
2018-11-28 13:28:09 -0500

    Load hubspot rake tasks
    Summary:
    Previously, this file was loading 'properties.rake' but the file was
    renamed to 'hubspot.rake'. This change restores the original behavior of
    exposing these rake tasks to end users.

--- lib/hubspot/railtie.rb
+++ lib/hubspot/railtie.rb
@@ -4,6 +4,7 @@ module Hubspot
   class Railtie < Rails::Railtie
     rake_tasks do
       spec = Gem::Specification.find_by_name('hubspot-ruby')
+      load "#{spec.gem_dir}/lib/tasks/hubspot.rake"
     end
   end
 end
