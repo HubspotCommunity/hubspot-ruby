dd1573a80247e2bfb876ffed3c8c004b6dd24045
Chris Bisnett <cbisnett@gmail.com>
2019-04-29 17:46:14 -0400

    Add Owner find method

--- lib/hubspot/owner.rb
+++ lib/hubspot/owner.rb
@@ -8,6 +8,7 @@ module Hubspot
   # TODO: Update an Owner
   # TODO: Delete an Owner
   class Owner
+    GET_OWNER_PATH    = '/owners/v2/owners/:owner_id' # GET
     GET_OWNERS_PATH   = '/owners/v2/owners' # GET
     CREATE_OWNER_PATH = '/owners/v2/owners' # POST
     UPDATE_OWNER_PATH = '/owners/v2/owners/:owner_id' # PUT
@@ -32,6 +33,12 @@ module Hubspot
         params   = { includeInactive: include_inactive }
         response = Hubspot::Connection.get_json(path, params)
         response.map { |r| new(r) }
+      end
+
+      def find(id, include_inactive=false)
+        response = Hubspot::Connection.get_json(path, owner_id: id,
+          include_inactive: include_inactive)
+        new(response)
       end
 
       def find_by_email(email, include_inactive=false)
